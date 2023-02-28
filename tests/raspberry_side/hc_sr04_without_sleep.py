6aed528c79e86f4b7532e36ced8641f6fd54ce1c
Samaze123456 <he202053@students.ephec.be>
2023-02-28 19:45:16 +0100

    add types to no sleep test

--- tests/raspberry_side/hc_sr04_without_sleep.py
+++ tests/raspberry_side/hc_sr04_without_sleep.py
@@ -1,74 +1,75 @@
 from machine import Pin
 import time
 
-number = 0
 
 def handle_echo(pin: Pin) -> None:
     global pulse_start
     global echo_received
     global pulse_end
-    pulse_end = time.ticks_us()
-    echo_received = True
+    pulse_end: int = time.ticks_us()
+    echo_received: bool = True
 
 def handle_trigger(pin: Pin) -> None:
     global pulse_start
-    pulse_start = time.ticks_us()
+    pulse_start: int = time.ticks_us()
 
-def write_distance(distance):
-    global number
-    print('n°{} | Distance: {:.0f} cm'.format(number, distance))
-    number += 1
+def write_distance(distance: float) -> None:
+    global number_of_trigger
+    print('n°{} | Distance: {:.0f} cm'.format(number_of_trigger, distance))
+    number_of_trigger += 1
 
-def calculate_distance(pulse_duration):
+def calculate_distance(pulse_duration: int) -> float:
     return round((pulse_duration * 17165 / 1000000) - 40, 0)
 
-def check_distance(distance):
+def check_distance(distance: float) -> bool:
     # global alarmed
     return distance < 30
     # if distance < 30:
     #     alarmed = True
     # alarmed = False
 
-def send_trigger(pin):
+def send_trigger(pin: Pin) -> None:
     pin.on()
     time.sleep_us(100)
     pin.off()
 
-pin_led_red = Pin(0, Pin.OUT, value=0)
-pin_led_green = Pin(1, Pin.OUT, value=0)
-pin_echo = Pin(16, Pin.IN, Pin.PULL_DOWN)
+pin_led_red: Pin = Pin(0, Pin.OUT, value=0)
+pin_led_green: Pin = Pin(1, Pin.OUT, value=0)
+pin_echo: Pin = Pin(16, Pin.IN, Pin.PULL_DOWN)
 pin_echo.irq(handle_echo, Pin.IRQ_FALLING)
-pin_trigger = Pin(17, Pin.OUT, value=0)
+pin_trigger: Pin = Pin(17, Pin.OUT, value=0)
 pin_trigger.irq(handle_trigger, Pin.IRQ_RISING)
-alarmed = False
-pulse_start = 0
-pulse_end = 0
-echo_received = False
-next_send_trigger = 0
-next_toggle_led_red = 0
+pulse_start: int = 0
+pulse_end: int = 0
+number_of_trigger: int = 0
+next_send_trigger: int = 0
+next_toggle_led_red: int = 0
+echo_received: bool = False
+alarmed: bool = False
+
 
 try:
     while True:
-        current_timestamp = time.ticks_us()
+        current_timestamp: int = time.ticks_us()
         
         # Time to send another trigger
         if time.ticks_diff(current_timestamp, next_send_trigger) >= 0:
             # print("sending")
             send_trigger(pin_trigger)
-            next_send_trigger = time.ticks_us() + 1000000
+            next_send_trigger: int = time.ticks_us() + 1000000
         
         # When echo is received
         if echo_received:
-            distance_cm = calculate_distance(pulse_end - pulse_start)
+            distance_cm: float = calculate_distance(pulse_end - pulse_start)
             write_distance(distance_cm)
-            alarmed = check_distance(distance_cm)
-            echo_received = False
+            alarmed: float = check_distance(distance_cm)
+            echo_received: bool = False
         
         # When the object is nearer than the limit
         if alarmed:
             pin_led_green.off()
             if time.ticks_diff(current_timestamp, next_toggle_led_red) >= 0:
-                next_toggle_led_red = time.ticks_us() + 500000
+                next_toggle_led_red: int = time.ticks_us() + 500000
                 pin_led_red.toggle()
         else:
             pin_led_red.off()
