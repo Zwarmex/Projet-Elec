e18a96f818f1b767945640762209a8655e0e31ed
Samuel Lambert <sam.f.lambert@gmail.com>
2023-05-15 15:03:18 +0200

    version final

--- code raspberry/hc_sr04_without_sleep.py
+++ code raspberry/hc_sr04_without_sleep.py
@@ -20,7 +20,6 @@ Pin 27 Echo
 Pin 28 Trig
 """
 
-
 from machine import Pin
 import time
 import sys
@@ -38,6 +37,7 @@ def handle_echo(pin: Pin) -> None:
         pulse_end = time.ticks_us()
         waiting_for_echo = True
 
+
 def write_distance(distance: str, displays_array: list) -> None:
     state_display_left: str = get_pins(int(distance[0]))
     state_display_right: str = get_pins(int(distance[1]))
@@ -45,31 +45,49 @@ def write_distance(distance: str, displays_array: list) -> None:
         displays_array[0][index].value(int(state_display_left[index]))
     for index in range(len(displays_array[1])):
         displays_array[1][index].value(int(state_display_right[index]))
-    print(f'{distance}')
-    
-def write_timeout(displays_array:list)->None:
-    pins_timeout: list=["0001110", '0011101']
+    print(f"{distance}")
+
+
+def write_timeout(displays_array: list) -> None:
+    pins_timeout: list = ["0001110", "0011101"]
     for indexDisplay in range(len(displays_array)):
         for indexPin in range(len(displays_array[indexDisplay])):
-            displays_array[indexDisplay][indexPin].value(int(pins_timeout[indexDisplay][indexPin]))
-            
-    print("Timeout")
-    
+            displays_array[indexDisplay][indexPin].value(
+                int(pins_timeout[indexDisplay][indexPin])
+            )
+
+    print("00")
+
+
 def get_pins(number: int) -> str:
-    pins_state: list = ["1111110", "0110000", "1101101", "1111001", "0110011",
-                        "1011011", "1011111", "1110000", "1111111", "1111011"]
+    pins_state: list = [
+        "1111110",
+        "0110000",
+        "1101101",
+        "1111001",
+        "0110011",
+        "1011011",
+        "1011111",
+        "1110000",
+        "1111111",
+        "1111011",
+    ]
     return pins_state[number]
+
 
 def calculate_distance(pulse_start: int, pulse_end: int) -> str:
     return "%02d" % (round(time.ticks_diff(pulse_end, pulse_start) / 58, 0))
 
+
 def check_alarm_and_meters(distance: str, alarm: int) -> bool:
     return int(distance) < alarm, int(distance) >= 100
+
 
 def send_trigger(pin: Pin) -> None:
     pin.on()
     time.sleep_us(10)
     pin.off()
+
 
 def handle_received_command(command: str) -> None:
     if "alarm=" in command:
@@ -78,20 +96,21 @@ def handle_received_command(command: str) -> None:
         alarm_limit = new_alarm_limit
         print(f"New alarm limit set to {alarm_limit}")
 
+
 #################INIT################
-led=Pin("LED", Pin.OUT, value=0)    #
-led(1)                              #
-time.sleep(0.5)                     #
-led(0)                              #
+led = Pin("LED", Pin.OUT, value=0)  #
+led(1)  #
+time.sleep(0.5)  #
+led(0)  #
 #####################################
 pin_led_red: Pin = Pin(0, Pin.OUT, value=0)
 pin_led_green: Pin = Pin(1, Pin.OUT, value=0)
 pin_display_dot: Pin = Pin(2, Pin.OUT, value=0)
-displays: list= [[],[]] # left, right
+displays: list = [[], []]  # left, right
 for i in range(7):
-    displays[0].append(Pin(9+i, Pin.OUT, value=0))
+    displays[0].append(Pin(9 + i, Pin.OUT, value=0))
 for i in range(7):
-    displays[1].append(Pin(16+i, Pin.OUT, value=0))
+    displays[1].append(Pin(16 + i, Pin.OUT, value=0))
 pin_echo: Pin = Pin(27, Pin.IN, Pin.PULL_DOWN)
 pin_echo.irq(handle_echo, Pin.IRQ_FALLING | Pin.IRQ_RISING)
 pin_trigger: Pin = Pin(28, Pin.OUT, value=0)
@@ -109,7 +128,7 @@ meters: bool = False
 try:
     while True:
         current_timestamp: int = time.ticks_us()
-        
+
         # Check if there's data to be read from the PC
         if pin_trigger.value() == 0 and select.select([sys.stdin], [], [], 0)[0]:
             received_data = sys.stdin.buffer.readline().strip()
@@ -118,10 +137,12 @@ try:
                 handle_received_command(received_command)
 
         # Time to send another trigger
-        if current_timestamp >= next_send_trigger :
+        if current_timestamp >= next_send_trigger:
             send_trigger(pin_trigger)
-            next_send_trigger: int = time.ticks_us() + 1000000
-            next_timeout_echo: int = time.ticks_us() + 100000 # time max travelled by sound for 800cm (400*2)
+            next_send_trigger: int = time.ticks_us() + 1_000_000
+            next_timeout_echo: int = (
+                time.ticks_us() + 100_000
+            )  # time max travelled by sound for 800cm (400*2)
             timeout_echo: bool = False
 
         # When the echo is in timeout
@@ -132,7 +153,7 @@ try:
             pin_led_red.on()
             pin_led_green.off()
             pin_display_dot.off()
-            
+
         if current_timestamp >= next_toggle_led_red:
             next_toggle_led_red: int = time.ticks_us() + 500000
             pin_led_red.toggle()
@@ -156,8 +177,9 @@ try:
                 else:
                     pin_display_dot.off()
                 pin_led_red.off()
-                pin_led_green.on()        
+                pin_led_green.on()
 
+# End Of Program
 except KeyboardInterrupt:
     pin_led_red.off()
     pin_led_green.off()
@@ -167,4 +189,4 @@ except KeyboardInterrupt:
     pin_display_dot.off()
     pin_echo.off()
     pin_trigger.off()
-    write("EOP")
+    print("EOP")
